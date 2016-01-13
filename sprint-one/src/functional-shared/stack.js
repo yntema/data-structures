042024a99753aa3ed8eebeb2d3e57092fbf5acbf
yntema <gerrityntema@gmail.com>
2016-01-13 15:14:58 -0800

    Returns a stack size of zero for everything using shared functional patterns

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,16 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.size = 0;
+  _.extend(newStack, stackMethods);
+
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.size = function () {
+  return 0;
+};
 
