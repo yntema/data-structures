e779470de0a7d1d8b5132f7ea01017cc81889fcd
yntema <gerrityntema@gmail.com>
2016-01-13 14:44:00 -0800

    Reports correct queue size after adding items

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,20 @@
 var Queue = function() {
   var someInstance = {};
-
+  var queueSize = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    queueSize++;
   };
 
   someInstance.dequeue = function() {
   };
 
   someInstance.size = function() {
-    return 0;
+    return queueSize;
   };
 
   return someInstance;
