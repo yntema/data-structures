93d1e0c825e218aa25cfb8642a8c13b5bd4cc3d8
yntema <gerrityntema@gmail.com>
2016-01-13 15:47:32 -0800

    Return stack size of zero for everything using prototypical patterns

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,15 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.stackSize = 0;
+
+  return newStack;
+
 };
 
 var stackMethods = {};
 
-
+stackMethods.size = function() {
+  return 0;
+};
