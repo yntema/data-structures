15bd51f564247dcd144827359316f5171cdeb63e
yntema <gerrityntema@gmail.com>
2016-01-13 15:52:59 -0800

    Implement sequential adding and removing to stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -3,7 +3,7 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
   var newStack = Object.create(stackMethods);
   newStack.stackSize = 0;
-
+  newStack.storage = {};
   return newStack;
 
 };
@@ -15,6 +15,7 @@ stackMethods.size = function() {
 };
 
 stackMethods.push = function(value) {
+  this.storage[this.stackSize] = value;
   this.stackSize++;
 };
 
@@ -22,4 +23,5 @@ stackMethods.pop = function() {
   if( this.stackSize > 0) {
     this.stackSize--;
   }
+  return this.storage[this.stackSize];
 };
