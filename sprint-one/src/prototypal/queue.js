b504674f51a2f94d0a0d6cd7e9109a81af2fac19
yntema <gerrityntema@gmail.com>
2016-01-13 15:56:20 -0800

    Return queue size of zero always

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,14 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.queueSize = 0;
+
+  return newQueue; 
 };
 
 var queueMethods = {};
 
-
+queueMethods.size = function() {
+  return 0;
+};
