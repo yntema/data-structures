95ab88bf1ef50a9f7c618bf4b855196cb0daef9b
yntema <gerrityntema@gmail.com>
2016-01-13 15:24:39 -0800

    Add all stack specific functionality

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -3,6 +3,7 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
   var newStack = {};
   newStack.stackSize = 0;
+  newStack.storage = {};
   _.extend(newStack, stackMethods);
 
   return newStack;
@@ -15,6 +16,7 @@ stackMethods.size = function () {
 };
 
 stackMethods.push = function(value) {
+  this.storage[this.stackSize] = value;
   this.stackSize++;
 };
 
@@ -22,4 +24,5 @@ stackMethods.pop = function() {
   if(this.stackSize > 0) {
     this.stackSize--;
   }
+  return this.storage[this.stackSize];
 };
