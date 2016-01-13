97a5ca1c132a0b04a1f924d876d498912c3c5261
yntema <gerrityntema@gmail.com>
2016-01-13 14:37:28 -0800

    Return correct stack size when adding items

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,20 @@
 var Stack = function() {
   var someInstance = {};
+  var stackSize = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    stackSize++;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
-    return 0;
+    return stackSize;
   };
 
   return someInstance;
