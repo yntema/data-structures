f0f8757341a6f3ee75c761d1fc1acdeeb533944d
yntema <gerrityntema@gmail.com>
2016-01-14 10:33:16 -0800

    Add children to tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,7 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
   _.extend(newTree, treeMethods);
 
   return newTree;
@@ -12,6 +12,7 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
