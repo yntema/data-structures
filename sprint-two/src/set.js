70f6371198dbae1ffb32ed32c46ee5aa868cc159
yntema <gerrityntema@gmail.com>
2016-01-14 14:10:23 -0800

    Add values to set and enable contain method

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,15 +1,17 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  return (item in this._storage);
 };
 
 setPrototype.remove = function(item) {
