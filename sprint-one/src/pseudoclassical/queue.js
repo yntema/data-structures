af89992b7a6b4fa4b904b161dc34e4b898bf11ea
yntema <gerrityntema@gmail.com>
2016-01-13 16:24:49 -0800

    Allow sequential adding and removing from queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -2,6 +2,7 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   this.queueSize = 0;
+  this.storage = {};
 };
 
 Queue.prototype.size = function() {
@@ -9,6 +10,7 @@ Queue.prototype.size = function() {
 };
 
 Queue.prototype.enqueue = function(value) {
+  this.storage[this.queueSize] = value;
   this.queueSize++;
 };
 
@@ -16,4 +18,5 @@ Queue.prototype.dequeue = function() {
   if(this.queueSize > 0) {
     this.queueSize--;
   }
+  return this.storage[this.queueSize];
 };
