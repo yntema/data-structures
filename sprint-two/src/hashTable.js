cd435740434cb7bd0b59ccd3143d72c8a8e1d818
yntema <gerrityntema@gmail.com>
2016-01-14 15:49:50 -0800

    Implement remove function for hash map

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -17,6 +17,7 @@ HashTable.prototype.retrieve = function(k) {
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
