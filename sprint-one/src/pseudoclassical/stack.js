9825dc69c4b0a73ee467e9b017d2388998b9ede0
yntema <gerrityntema@gmail.com>
2016-01-13 16:11:31 -0800

    Use pseudoclassical patterns to return zero for stack size always

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,10 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.stackSize = 0;
+
 };
 
-
+Stack.prototype.size = function() {
+  return 0;
+};
