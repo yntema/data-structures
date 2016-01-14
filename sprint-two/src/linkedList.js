6ef196d7295c2eac371101a0b406805c8b60435e
yntema <gerrityntema@gmail.com>
2016-01-13 16:40:28 -0800

    addTail changes value of tail node

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,11 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    list.tail = Node(value);
   };
 
   list.removeHead = function() {
+    
   };
 
   list.contains = function(target) {
