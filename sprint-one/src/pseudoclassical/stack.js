52267b81236db9d56c4a2839fdeb5acb6e521355
yntema <gerrityntema@gmail.com>
2016-01-13 16:16:46 -0800

    sequentially add and remove from stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -2,7 +2,7 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   this.stackSize = 0;
-
+  this.storage = {};
 };
 
 Stack.prototype.size = function() {
@@ -10,6 +10,7 @@ Stack.prototype.size = function() {
 };
 
 Stack.prototype.push = function(value) {
+  this.storage[this.stackSize] = value;
   this.stackSize++;
 };
 
@@ -17,4 +18,5 @@ Stack.prototype.pop = function() {
   if(this.stackSize > 0) {
     this.stackSize--;
   }
+  return this.storage[this.stackSize];
 };
