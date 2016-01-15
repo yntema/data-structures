8935aac543c4a61afc3f42aee47998aa39220887
yntema <gerrityntema@gmail.com>
2016-01-14 16:32:48 -0800

    Create insert, contains, depthfirstlog methods for tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,23 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(BinarySearchTree.prototype);
+
+  return tree;
 };
 
 
+BinarySearchTree.prototype.insert = function() {
+
+};
+
+
+BinarySearchTree.prototype.contains = function() {
+
+};
+
+
+BinarySearchTree.prototype.depthFirstLog = function() {
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
