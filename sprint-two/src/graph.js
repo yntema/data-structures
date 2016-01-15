835674aa3a8fc671beb90df8f9254c5c44caea70
yntema <gerrityntema@gmail.com>
2016-01-14 20:02:57 -0800

    Time complexity for graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -67,5 +67,12 @@ Graph.prototype.forEachNode = function(cb) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ // addNode: O(1)
+ // contains: O(1)
+ // removeNode: O(1)
+ // hasEdge: O(n)
+ // addEdge: O(1)
+ // removeEdge: O(n)
+ // forEachNode: O(n)
 
 
