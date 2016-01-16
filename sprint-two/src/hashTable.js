1094f4378e0411bb32cec7028cb30c6cf443b26d
yntema <gerrityntema@gmail.com>
2016-01-16 10:14:47 -0800

    Resize Hash table as needed

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,9 +3,14 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.count = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
+  if (this.count / this._limit >= 0.75) {
+    // resize array
+    this.resize(this._limit*2);
+  }
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
   var exists = false;
@@ -20,6 +25,7 @@ HashTable.prototype.insert = function(k, v) {
   if(!exists) {
     bucket.push([k, v]);
   }
+  this.count++;
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -31,21 +37,53 @@ HashTable.prototype.retrieve = function(k) {
 };
 
 HashTable.prototype.remove = function(k) {
+
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
   this.bucketFind(bucket, k,  function(item) {
       item[1] = undefined;
   });
-};
+  if (this.count > 0) {
+    this.count--;
+  }
 
-HashTable.prototype.bucketFind = function(bucket, key, func) {
-  for (var i = 0; i < bucket.length; i++) {
-    if(bucket[i][0] === key) {
-      return func(bucket[i]);
-    }
+  if (this.count / this._limit < 0.25) { // maybe <= ???
+    // resize array
+    this.resize(Math.floor(this._limit/2));
   }
 };
 
+HashTable.prototype.bucketFind = function(bucket, key, func) {
+  if(bucket) {
+    for (var i = 0; i < bucket.length; i++) {
+      if(bucket[i] && bucket[i][0] === key) {
+        return func(bucket[i]);
+      }
+    }    
+  }
+};
+
+HashTable.prototype.resize = function(newSize) {
+  // input: number
+  // copy storage object to oldStorage variable
+  var oldStorage = this._storage;
+  // create new limited array with size newSize
+  this._storage = LimitedArray(newSize);
+  // set new limit to newSize
+  this._limit = newSize;
+  // begin rehash function get new index for each item and insert it in new limited array
+  // loop through each bucket in oldStorage array 
+  _.each(this._storage, function(bucket) {
+    // loop through each tuple in buckets
+    _.each(bucket, function(tuple) {
+      // add key value pair to new storage using insert method
+      if(tuple) {
+        this.insert(tuple[0], tuple[1]);
+      }
+    });
+  });
+  // output:none
+};
 
 
 /*
